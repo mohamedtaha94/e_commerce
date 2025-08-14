decc66bb42cb9f88cedab329da3c86b5db12ae74
mohamedtaha94 <104304060+mohamedtaha94@users.noreply.github.com>
2025-08-14 11:41:17 +0300

    modification

--- lib/presentation/settings/pages/settings.dart
+++ lib/presentation/settings/pages/settings.dart
@@ -1,37 +1,5 @@
-/*
-import 'package:e_commerce/common/widgets/appbar/app_bar.dart';
-import 'package:e_commerce/presentation/settings/widgets/my_orders_tile.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import '../widgets/my_favorties_tile.dart';
-
-class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: BasicAppbar(
-        title: Text(
-          'Settings'
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.r),
-        child: Column(
-          children: [
-            MyFavortiesTile(),
-            SizedBox(height: 15.h,),
-            MyOrdersTile()
-          ],
-        ),
-      ),
-    );
-  }
-}*/
 import 'package:e_commerce/common/widgets/appbar/app_bar.dart';
 import 'package:e_commerce/presentation/auth/pages/siginin.dart';
-import 'package:e_commerce/presentation/home/pages/home.dart';
 import 'package:e_commerce/presentation/settings/widgets/my_orders_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -54,8 +22,6 @@ class SettingsPage extends StatelessWidget {
             SizedBox(height: 15.h),
             MyOrdersTile(),
             SizedBox(height: 15.h),
-
-            // Logout button
             ListTile(
               leading: const Icon(Icons.logout, color: Colors.red),
               title: const Text(
@@ -63,10 +29,6 @@ class SettingsPage extends StatelessWidget {
                 style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
               ),
               onTap: () async {
-                // If you use Firebase Auth
-                // await FirebaseAuth.instance.signOut();
-
-                // Navigate to HomePage (replace current stack)
                 Navigator.pushAndRemoveUntil(
                   context,
                   MaterialPageRoute(builder: (context) => const SigninPage()),
